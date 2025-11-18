13b7e8d1a674df2482ece8c204dce9b56a86451f
1F10230288 <s1f102302887@iniad.org>
2025-11-19 02:57:12 +0900

    修正

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
-from . import views
+from .views import (
+    ProductListView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+)
 
 app_name = 'product'
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list'),
-    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    # 商品一覧
+    path('', ProductListView.as_view(), name='product_list'),
+
+    # 商品詳細
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+
+    # 商品作成
+    path('create/', ProductCreateView.as_view(), name='product_form'),
+
+    # 商品更新
+    path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+
+    # 商品削除
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- product/views.py
+++ product/views.py
@@ -7,20 +7,21 @@ from .models import Product
 # 商品一覧
 class ProductListView(generic.ListView):
     model = Product
-    template_name = 'product/list.html'
+    template_name = 'product/product_list.html'
     context_object_name = 'products'
 
 # 商品詳細
 class ProductDetailView(generic.DetailView):
     model = Product
-    template_name = 'product/detail.html'
+    template_name = 'product/product_detail.html'
     context_object_name = 'product'
 
 # 商品作成
 class ProductCreateView(LoginRequiredMixin, generic.edit.CreateView):
     model = Product
     fields = ['name', 'description', 'price', 'image', 'category']
-    template_name = 'product/form.html'
+    template_name = 'product/product_form.html'
+    success_url = reverse_lazy('product:product_list')
 
     def form_valid(self, form):
         form.instance.craftsman = self.request.user
@@ -30,7 +31,7 @@ class ProductCreateView(LoginRequiredMixin, generic.edit.CreateView):
 class ProductUpdateView(LoginRequiredMixin, generic.edit.UpdateView):
     model = Product
     fields = ['name', 'description', 'price', 'image', 'category']
-    template_name = 'product/form.html'
+    template_name = 'product/product_form.html'
 
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
@@ -41,7 +42,7 @@ class ProductUpdateView(LoginRequiredMixin, generic.edit.UpdateView):
 # 商品削除
 class ProductDeleteView(LoginRequiredMixin, generic.edit.DeleteView):
     model = Product
-    template_name = 'product/delete.html'
+    template_name = 'product/product_delete.html'
     success_url = reverse_lazy('product:product_list')
 
     def dispatch(self, request, *args, **kwargs):
